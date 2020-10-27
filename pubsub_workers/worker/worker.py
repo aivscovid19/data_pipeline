e8cb603b527cfc15dda03fa227b10efb41916a31
Simon Ewing <simonsays095@gmail.com>
2020-10-26 20:27:21 -0500

    Updated pubsub worker

--- pubsub_workers/worker/worker.py
+++ pubsub_workers/worker/worker.py
@@ -1,55 +1,23 @@
 #!/usr/bin/env python
-import pika
-import time
 import json
-from os import environ
 from urllib.parse import urlparse
-import uuid
 from datetime import datetime
-from google.cloud import bigquery
+from google.cloud import pubsub_v1
 from tables import StatusTable, DataTable
 
-self_id = str(uuid.uuid1())  # Use uuid1 so the network ID is in the uuid, and we can trace it back to this machine.
-
-# extract environment variables
-mq_user = environ.get('RABBIT_USERNAME')
-mq_pass = environ.get('RABBIT_PASSWORD')
-host_ip = environ.get('RABBIT_HOST_IP')
-#project_id = environ.get('PROJECT_ID')
-
-assert None not in [mq_user, mq_pass, host_ip], "Include a .env file using the docker argument --env-file when running."
-
-credentials = pika.PlainCredentials('guest','guest')
-#credentials = pika.PlainCredentials(mq_user, mq_pass)
-
-connection = pika.BlockingConnection(
-    #pika.ConnectionParameters(host='35.223.198.161', credentials=credentials))
-    pika.ConnectionParameters(host=host_ip, credentials=credentials))
-channel = connection.channel()
-
-channel.queue_declare(queue='task_queue', durable=True)
-print(' [*] Waiting for messages. To exit press CTRL+C', flush=True)
 
 statusTable = StatusTable().GetOrCreate()
 dataTable = DataTable().GetOrCreate()
 
-#bq_client = bigquery.Client()
-#table = bq_client.get_table('urls.status')
 
-def callback(ch, method, properties, body):
-    status = json.loads(body)
-    print("\n [x] Received %r" % body, flush=True)
+def callback(message):
+    status = json.loads(message.decode("utf-8"))
+    print(f"\n [x] Received {message}", flush=True)
 
     # Tell bq that we received the request
     status['status'] = 'Started Mining'
     status['timestamp'] = datetime.utcnow()
     status['worker_id'] = self_id
-    #bq_client.insert_rows(
-    #        table = table,
-    #        rows = [status]
-    #)
-    print("status:")
-    print(status)
     errors = statusTable.insert_row(status)
     if errors != []:
         print(f"We've got some errors when updating bq: {errors}", flush=True)
@@ -57,7 +25,6 @@ def callback(ch, method, properties, body):
     # Do the actual mining
     import miners
     print("getting article info from", status['article_url'], flush=True)
-    #domain = status['catalog_url'].split(".")[0]
     domain = urlparse(status['article_url']).netloc.split('.')[1]  # Get rid of the extension and subdomain
     print("Using domain:", domain, flush=True)
     miner = getattr(miners, domain.lower(), None)
@@ -66,7 +33,7 @@ def callback(ch, method, properties, body):
         status['status'] = 'Failed'
         status['timestamp'] = datetime.utcnow()
         errors = statusTable.insert_row(status)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+        message.ack()
         if errors != []:
             print(f"We've got some errors when updating bq: {errors}", flush=True)
         return
@@ -78,11 +45,11 @@ def callback(ch, method, properties, body):
         errors = statusTable.insert_row(status)
         if errors != []:
             print(f"We've got some errors when updating bq: {errors}", flush=True)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+        message.ack()
         return
     data['language'] = status['language']  # Should always be true...
     print("\nGot data:", flush=True)
-    print(*data.items(), sep='\n')
+    print(*data.items(), sep='\n', flush=True)
 
     # Send results to the data table
     errors = dataTable.insert_row(data)
@@ -98,10 +65,36 @@ def callback(ch, method, properties, body):
 
     print(" [x] Done", flush=True)
     print(' [*] Waiting for messages. To exit press CTRL+C', flush=True)
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    message.ack()
 
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='task_queue', on_message_callback=callback)
+if __name__ == "__main__":
+    from os import environ
+    import uuid
 
-channel.start_consuming()
+    self_id = str(uuid.uuid1())  # Use uuid1 so the network ID is in the uuid, and we can trace it back to this machine.
+
+    # extract environment variables
+    project_id = environ.get('PROJECT_ID')
+    subcription_ID = environ.get('PUBSUB_VERIFICATION_TOKEN')
+
+    assert project_id is not None, "Include a .env file using the docker argument --env-file when running."
+
+    print(' [*] Waiting for messages. To exit press CTRL+C', flush=True)
+
+    # Create a subscriber
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path(project_id, subcription_ID)
+
+    # Subscribe
+    streaming_pull_future = subscriber.subscribe(
+        subscription_path,
+        callback=callback
+    )
+    print(f"Listening for messages on {subscription_path}", flush=True)
+
+    # Stop the thread from quitting until the job is finished
+    try:
+        streaming_pull_future.result()
+    except Exception:
+        streaming_pull_future.cancel()
