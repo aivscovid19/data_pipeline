da1c28a35ef6a5b6cda8613e8b068dff50287b44
Yeshwanth <39859311+yeshwanth16@users.noreply.github.com>
2020-11-30 11:20:21 +0530

    Update url_builder_integration.py

--- Integrated_URL_builder/url_builder_integration.py
+++ Integrated_URL_builder/url_builder_integration.py
@@ -42,79 +42,75 @@ class URL_builder():
     self.total_urls=[]
     self.valid_urls=[]
     self._search_word=search_word
+    self.urls_dataframe = pd.DataFrame()
     self._driver_path = '/usr/lib/chromium-browser/chromedriver'
-    self._anchor_element={'arxiv':'span.list-identifier > a','biorxiv':'a.highwire-cite-linked-title','medrxiv':'a.highwire-cite-linked-title','preprint':'a#title.title',
-            'pbmc':'a.class1','jamanetwork':'h3.article--title > a'}
+    self._anchor_element={'arxiv':'span.list-identifier > a',
+                          'biorxiv':'a.highwire-cite-linked-title',
+                          'medrxiv':'a.highwire-cite-linked-title',
+                          'preprint':'a#title.title',
+                          'pbmc':'a.class1','jamanetwork':'h3.article--title > a'}
 
     self._url_base={'arxiv':f"http://export.arxiv.org/find/all/1/all:+{search_word}/0/1/0/all/0/1?skip=".format(search_word),
               'biorxiv':f'https://www.biorxiv.org/search/{search_word}%20numresults%3A75%20sort%3Apublication-date%20direction%3Adescending?page='.format(search_word),
               'medrxiv':f'https://www.medrxiv.org/search/{search_word}%20numresults%3A75%20sort%3Apublication-date%20direction%3Adescending?page='.format(search_word),
               'preprint':f"https://www.preprints.org/search?search1={search_word}&field1=article_abstract&field2=authors&clause=AND&search2=&page_num=".format(search_word),
-              'jamanetwork':f'https://jamanetwork.com/searchresults?q={search_word}&sort=Newest&page='.format(search_word),
-              'pbmc':f'http://pbmc.ibmc.msk.ru/ru/search-ru/?search={search_word}&fn=5'.format(search_word)             
+              'pbmc':f'http://pbmc.ibmc.msk.ru/ru/search-ru/?search={search_word}&fn=5'.format(search_word),
+              'jamanetwork':f'https://jamanetwork.com/searchresults?q={search_word}&sort=Newest&page='.format(search_word)
               }
 
 
     self._schema = [
-            {'name': 'article_url', 'type': 'STRING',   'mode': 'REQUIRED'},
-            {'name': 'catalog_url', 'type': 'STRING',   'mode': 'REQUIRED'},
-            {'name': 'is_pdf',      'type': 'INTEGER',  'mode': 'REQUIRED'},
-            {'name': 'language',    'type': 'STRING',                     },
-            {'name': 'status',      'type': 'STRING',   'mode': 'REQUIRED'},
+            {'name': 'article_url', 'type': 'STRING',    'mode': 'REQUIRED'},
+            {'name': 'catalog_url', 'type': 'STRING',    'mode': 'REQUIRED'},
+            {'name': 'is_pdf',      'type': 'INTEGER',   'mode': 'REQUIRED'},
+            {'name': 'language',    'type': 'STRING',                      },
+            {'name': 'status',      'type': 'STRING',    'mode': 'REQUIRED'},
             {'name': 'timestamp',   'type': 'TIMESTAMP', 'mode': 'REQUIRED'},
-            {'name': 'worker_id',   'type': 'STRING',                     },
-            {'name': 'meta_info',   'type': 'STRING',                     },
+            {'name': 'worker_id',   'type': 'STRING',                      },
+            {'name': 'meta_info',   'type': 'STRING',                      },
     ]
   
 
 
 
   @classmethod
-  def connect_to_gbq(cls,credentials,project_id,table_id):
+  def connect_to_gbq(cls,project_id,table_id):
     """ 
       Establish a connection with Google BigQuery
         Args:
-            credentials (:obj: `google.auth.credentials.Credentials`):
-                Google Authentication credentials object, required to
-                authorize the data workflow between this class and the
-                database. Can be declared from service or user account.
             project_id (string): Google Cloud project ID.
             table_id (string): Table information to read and write data on.
                 Must be specified as `<dataset_name>.<table_name>`.
-            schema (`list` of `dict`: optional): Optional databse schema to
-                input while working with `pandas_gbq` module. If `None`, the
-                schema will be inferred from `pandas.DataFrame` object.
         Example:
-            URL_builder.connect_to_gbq(google-credentials, 'for-yr',
-              'my_dataset.my_table').
+            URL_builder.connect_to_gbq('for-yr','my_dataset.my_table').
         Auth_docs:
             Reference for Google BigQuery Authentication, on this context:
             'https://pandas-gbq.readthedocs.io/en/latest/howto/authentication.html'
     """
-    cls.credentials=credentials
     cls.project_id=project_id
     cls.table_id=table_id
 
 
 
   @classmethod
-  def urlbuilderfactory(cls,journal_name,search_word,limit):
+  def urlbuilderfactory(cls,journal_name,search_word,limit,time_frame):
     '''
       Scrapes URLs from given journal name.
         Args:
-            journal_name: Name of the journal from which URLs need to be scraped.
-            search_word: keyword to be searched.
-            limit: number of URLs to be scraped.
+            journal_name(str): Name of the journal from which URLs need to be scraped.
+            search_word(str): keyword to be searched.
+            limit(int): number of URLs to be scraped.
+            time_frame(float): waiting time period to send dataframe to bigquery.
         Example:
-            URL_builder.urlbuilderfactory('arxiv','virus',1000)
+            URL_builder.urlbuilderfactory('arxiv','virus',1000,10)
     '''
-    journaldictionary={'arxiv':arxiv_url(search_word,limit),
-                       'preprint':preprint_url(search_word,limit),
-                       'biorxiv':biorxiv_url(search_word,limit),
-                       'medrxiv':medrxiv_url(search_word,limit),
-                       'jamanetwork': jamanetwork_url(search_word,limit),
-                       'pbmc':pbmc_url(search_word,limit),
-                       'scielo':scielo_url(search_word,limit)
+    journaldictionary={'arxiv':arxiv_url(search_word,limit,time_frame),
+                       'preprint':preprint_url(search_word,limit,time_frame),
+                       'biorxiv':biorxiv_url(search_word,limit,time_frame),
+                       'medrxiv':medrxiv_url(search_word,limit,time_frame),
+                       'pbmc':pbmc_url(search_word,limit,time_frame),
+                       'scielo':scielo_url(search_word,limit,time_frame),
+                       'jamanetwork': jamanetwork_url(search_word,limit,time_frame)
                        }
     return journaldictionary[journal_name]
 
@@ -123,7 +119,7 @@ class URL_builder():
 
   def _url_collector(self):
     '''
-      Method to scrape the article URLs
+      Method to scrape the article URLs from a given page.
     '''
     if self.page_num is not None:
       self.page_url=self._url_base[self.journal]+str(self.page_num)
@@ -137,7 +133,7 @@ class URL_builder():
   
   def _create_url_schema(self,list_of_urls,catalog_url,is_pdf=None,lang='en'):
     '''
-      Method to create a dataframe.
+      Method to create a dataframe for a set of urls.
 
       Args:
         list_of_urls(list): URLs scraped.
@@ -155,7 +151,7 @@ class URL_builder():
     else:
       self._url_schema['is_pdf'] = self._url_schema['article_url'].apply(is_pdf)
     self._url_schema['language']=lang
-    self._url_schema['status']='Not mined'
+    self._url_schema['status']='Not Mined'
     self._url_schema['timestamp']= datetime.utcnow()
     self._url_schema['worker_id']=None
     self._url_schema['meta_info']=None
@@ -167,12 +163,12 @@ class URL_builder():
     
   def _send_to_bigquery(self):
     '''
-        Method to push urls to bigquery.
+        Method to send dataframe to bigquery.
     '''
-    credentials = service_account.Credentials.from_service_account_file(self.credentials)
-    pandas_gbq.to_gbq(self._url_schema, self.table_id, project_id=self.project_id, if_exists='append', table_schema=self._schema,credentials=credentials)
+    pandas_gbq.to_gbq(self.urls_dataframe, self.table_id, project_id=self.project_id, if_exists='append', table_schema=self._schema)
     pass
   
+
 
 
   def get_urls(self):
@@ -180,25 +176,43 @@ class URL_builder():
       Method to get URLs and send url_schema to bigquery.
 
       Returns: url_schema
+
     '''
-    for self.page_num in range(0,99999):
+    self.page_num=0
+    start_time=time.time()
+    check_urls=[]
+    while self.page_num >=0:
       print(f"\n Scraping from page...", flush=True)
       self._url_collector()
 
       [self.valid_urls.append(i) for i in self.urls]  
-      nums=min(self.limit - len(self.total_urls), len(self.valid_urls))
-      self.total_urls.extend(self.valid_urls[:nums])
-
+      nums=min(self.limit - len(self.total_urls), len(self.urls))
+      self.total_urls.extend(self.valid_urls[:nums])    
       self._url_list=[]
-      [self._url_list.append(i) for i in self.valid_urls[:nums]]
+      [self._url_list.append(i) for i in self.urls[:nums]]
       self._create_url_schema(self._url_list,self.page_url)
-      self._send_to_bigquery()
+    
+
+
+
+      self.urls_dataframe = pd.concat([self.urls_dataframe,self._url_schema],ignore_index=True)
+      if (time.time()-start_time)>=self.time_frame:
+        print(self.urls_dataframe)
+        self._send_to_bigquery()
+        self.urls_dataframe=pd.DataFrame()
+        start_time=time.time()
+        
+
+      
       if len(self.total_urls) == self.limit:
         break
-    print('\n Total no. of URLS:'+str(len(self._url_list)))
+      self.page_num+=1
+
+    self._send_to_bigquery()
+    print(self.urls_dataframe)
+    print('\n Total no. of URLS sent:'+str(len(self._url_list)))
     return self._url_schema
-  
-  
+
 
 
 
@@ -212,11 +226,13 @@ class arxiv_url(URL_builder):
       Args:
         search_word(str): word to be searched
         limit(int): number of URLs to be collected
+        time_frame(float): waiting time period to send dataframe to bigquery
   """
 
-  def __init__(self,search_word,limit):
+  def __init__(self,search_word,limit,time_frame):
         super().__init__(search_word)  
         self.limit=limit
+        self.time_frame=time_frame
   
 
   def get_urls(self):
@@ -226,23 +242,37 @@ class arxiv_url(URL_builder):
       Returns: url_schema
 
     '''
-    for self.page_num in range(0,25*(99999),25):
+    self.page_num=0
+    start_time=time.time()
+    while self.page_num>=0:
       self.journal='arxiv'
-      print(f"\n Scraping from page...", flush=True)
+      self.valid_urls=[]
+      print(f"\n Scraping from page {self.page_num}...", flush=True)
       is_pdf = lambda x: pd.Series({'is_pdf': 1 if x.find('pdf') != -1 else 0}) 
       self._url_collector()
 
       [self.valid_urls.append(i) for i in self.urls if 'format' not in i]   
       nums=min(self.limit - len(self.total_urls), len(self.valid_urls))
       self.total_urls.extend(self.valid_urls[:nums])
-
       self._url_list=[]
       [self._url_list.append(i) for i in self.valid_urls[:nums]]
       self._create_url_schema(self._url_list,self.page_url,is_pdf)
-      self._send_to_bigquery()
 
+
+      self.urls_dataframe = pd.concat([self.urls_dataframe,self._url_schema],ignore_index=True)
+      if (time.time()-start_time)>=self.time_frame:
+        self._send_to_bigquery()
+        print(self.urls_dataframe)
+        self.urls_dataframe=pd.DataFrame()
+        start_time=time.time()
+
+      
+      self.page_num+=25      
       if len(self.total_urls) == self.limit:
         break
+
+  
+    self._send_to_bigquery()
     print('\n Total no. of URLS:'+str(len(self._url_list)))
     return self._url_schema
 
@@ -257,11 +287,13 @@ class preprint_url(URL_builder):
       Args:
         search_word(str): word to be searched
         limit(int): number of URLs to be collected
+        time_frame(float): waiting time period to send dataframe to bigquery
   """
 
-  def __init__(self,search_word,limit):
+  def __init__(self,search_word,limit,time_frame):
       super().__init__(search_word)  
       self.limit=limit
+      self.time_frame=time_frame
       self.journal='preprint'
   
     
@@ -276,16 +308,16 @@ class biorxiv_url(URL_builder):
       Args:
         search_word(str): word to be searched
         limit(int): number of URLs to be collected
+        time_frame(float): waiting time period to send dataframe to bigquery
   """
 
-  def __init__(self,search_word,limit):
+  def __init__(self,search_word,limit,time_frame):
       super().__init__(search_word)  
       self.limit=limit
+      self.time_frame=time_frame
       self.journal='biorxiv'
   
   
-
-
 
 
 class medrxiv_url(URL_builder):
@@ -296,14 +328,15 @@ class medrxiv_url(URL_builder):
       Args:
         search_word(str): word to be searched
         limit(int): number of URLs to be collected
+        time_frame(float): waiting time period to send dataframe to bigquery
   """
 
-  def __init__(self,search_word,limit):
+  def __init__(self,search_word,limit,time_frame):
       super().__init__(search_word)  
       self.limit=limit
       self.journal='medrxiv'
+      self.time_frame=time_frame
   
-
 
 
 class jamanetwork_url(URL_builder):
@@ -314,15 +347,15 @@ class jamanetwork_url(URL_builder):
       Args:
         search_word(str): word to be searched
         limit(int): number of URLs to be collected
+        time_frame(float): waiting time period to send dataframe to bigquery
   """
 
-  def __init__(self,search_word,limit):
+  def __init__(self,search_word,limit,time_frame):
       super().__init__(search_word)  
       self.limit=limit
       self.journal = 'jamanetwork'
+      self.time_frame=time_frame
   
-  
-
 
 class pbmc_url(URL_builder):
   """
@@ -332,12 +365,14 @@ class pbmc_url(URL_builder):
       Args:
         search_word(str): word to be searched
         limit(int): number of URLs to be collected
+        time_frame(float): waiting time period to send dataframe to bigquery
   """
 
-  def __init__(self,search_word,limit):
+  def __init__(self,search_word,limit,time_frame):
     super().__init__(search_word)
     self.limit=limit
     self.journal='pbmc'
+    self.time_frame=time_frame
   
   def get_urls(self):
     '''
@@ -358,7 +393,6 @@ class pbmc_url(URL_builder):
     return self._url_schema
 
 
-
 class scielo_url(URL_builder):
   """
       Child class for getting URLs from preprint journal.
@@ -367,11 +401,13 @@ class scielo_url(URL_builder):
       Args:
         search_word(str): word to be searched
         limit(int): number of URLs to be collected
+        time_frame(float): waiting time period to send dataframe to bigquery
   """
 
-  def __init__(self,search_word,limit):
+  def __init__(self,search_word,limit,time_frame):
     super().__init__(search_word)
     self.limit=limit
+    self.time_frame=time_frame
 
   def get_urls(self):
     '''
@@ -379,11 +415,12 @@ class scielo_url(URL_builder):
 
       Returns: url_schema
     '''
-    print(f"\n Scraping from page...", flush=True)
+    print('scielo')
     miner = mining.MiningEngine(ScieloSearchLocations, driver_path=self._driver_path)
     ScieloURLBuilder.connect_to_gbq(self.credentials, self.project_id, self.table_id, self._schema)
     scielo_builder = ScieloURLBuilder(miner)
     scielo_builder.collect('https://search.scielo.org/', self._search_word, self.limit)
+
 
     
     
@@ -394,9 +431,9 @@ if __name__ == "__main__":
     url_table_id = os.environ["TABLE_ID"]
     search_item  = os.environ["SEARCH_WORD"]
     limit        = int(os.environ["LIMIT"])
-    credentials  = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+    time_frame   = float(os.environ["TIME_FRAME"])
 
-    URL_builder.connect_to_gbq(credentials,project_id, url_table_id)
+    URL_builder.connect_to_gbq(project_id, url_table_id)
 
-    url=URL_builder.urlbuilderfactory(domain,search_item,limit)
+    url=URL_builder.urlbuilderfactory(domain,search_item,limit,time_frame)
     url.get_urls()
