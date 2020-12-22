cde68f5b5810bf581b67d2daef96275f7af016d9
Simon Ewing <Simonsays095@gmail.com>
2020-12-22 16:46:00 +0000

    removed commented lines

--- pubsub_workers_integrated/worker/worker.py
+++ pubsub_workers_integrated/worker/worker.py
@@ -47,7 +47,6 @@ def callback(message):
     if data is None:
         print("Mining returned no results.", flush=True)
         status['status'] = "No results"
-        #status['timestamp'] = datetime.utcnow()
         status['timestamp'] = datetime.now(timezone.utc)
         errors = statusTable.insert_row(status)
         if errors != []:
@@ -83,27 +82,18 @@ if __name__ == "__main__":
     self_id = str(uuid.uuid1())  # Use uuid1 so the network ID is in the uuid, and we can trace it back to this machine.
 
     # extract environment variables
-    # project_id = environ.get('PROJECT_ID')
     project_id = DataTable.table_id.split('.')[0]
     subcription_ID = environ.get('PUBSUB_VERIFICATION_TOKEN')
-    # topic_ID = environ.get('PUBSUB_TOPIC')
 
     assert project_id is not None, "Include a .env file using the docker argument --env-file when running."
 
     print(' [*] Waiting for messages. To exit press CTRL+C', flush=True)
 
     # Create a subscriber
-    # publisher = pubsub_v1.PublisherClient()
     subscriber = pubsub_v1.SubscriberClient()
-    # topic_path = publisher.topic_path(project_id, topic_ID)
     subscription_path = subscriber.subscription_path(project_id, subcription_ID)
 
     flow_control = pubsub_v1.types.FlowControl(max_messages=1)
-    # with subscriber:
-    #    subscription = subscriber.create_subscription(
-    #        request = {"name": subscription_path, "topic": topic_path}
-    #    )
-    #    print("subscribed", flush=True)
 
     # Subscribe
     streaming_pull_future = subscriber.subscribe(
